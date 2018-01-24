a59a09d0a180a047417e9985c6749ee08f00b12f
omarnasir <momar.nasir@gmail.com>
2018-01-24 19:36:48 +0200

    minor bug fix for folder creation

--- midi-featurization.py
+++ midi-featurization.py
@@ -62,12 +62,10 @@ def remove_zeros(feature_vec):
     return np.delete(feature_vec, zero_indices, axis=1)
 
 def main():
-    artist = sys.argv[1]
-    parent_path = ''
-    
-    if sys.argv[2] is not None:
-        parent_path = sys.argv[2]
-    midifiles = glob.glob(parent_path + './data/artist*.mid')
+    artist = 'beethoven'
+    parent_path = 'D:/DeepLearning'
+
+    midifiles = glob.glob(parent_path + '/data/' + artist + '/*.mid')
     for midifile in midifiles:   
         audio = midi.read_midifile(midifile)
         instrument_indices = np.zeros(len(audio), dtype=int)
@@ -78,13 +76,16 @@ def main():
             if feature_vec is not None:
                 feature_vec_sanitized = remove_zeros(feature_vec)
                 df = pd.DataFrame(feature_vec_sanitized)
-                file_name = parent_path + './output/artist' + str(instrument) + '_' + artist + '_'
+                file_name = parent_path + '/output/' + artist + '/' + str(instrument) + '_' + artist + '_'
 
-                # Check if file exists, and set index appropriately
+                # Check if file/folder exists, and set index appropriately
+                if not os.path.isdir(parent_path + '/output/' + artist + '/'):
+                    os.makedirs(parent_path + '/output/' + artist + '/')
                 if os.path.isfile(file_name + str(instrument_indices[index]) + '.gz'):
                     instrument_indices[index] += 1
+
                 # Write file to output folder
-                df.to_csv(file_name + str(instrument_indices[index]) + '.gz', compression='gzip')
+                df.to_csv(file_name + str(instrument_indices[index]) + '.csv.gz', compression='gzip')
             
 if __name__ == "__main__":
     main()
