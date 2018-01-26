f36f3201fcc83c1cea4c54186ea1d3b9e6ccea0a
omarnasir <momar.nasir@gmail.com>
2018-01-27 01:01:24 +0200

    LSTM training Model working now

--- data_loader.py
+++ data_loader.py
@@ -33,12 +33,12 @@ def load_data(sequence_size, step_size):
     # Compute input vector size
     input_range = (df.shape[1] - sequence_size) // step_size
 
-    for i in range(0,input_range,step_size):
+    for i in range(1,input_range,step_size):
         input_seq.append(df.values[:, i:i+sequence_size].ravel().reshape(128,sequence_size))
         target.append(df.values[:,i+sequence_size:i+sequence_size+1].ravel().reshape(128,1))
 
     print("Completed")
-    return input_seq, target
+    return input_seq, np.array(target,dtype=int)
 
 if __name__ == "__main__":
     load_data(12, 6)
--- lstm.py
+++ lstm.py
@@ -7,52 +7,68 @@ import torch.optim as optim
 
 from data_loader import load_data
 
+class LSTM_Net(nn.Module):
+    def __init__(self, D_in, hidden_dim, num_layers, D_out, num_sequences):
+        super(LSTM_Net,self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.num_sequences = num_sequences
+
+        self.lstm = nn.LSTM(D_in, hidden_dim, num_layers, dropout=0.2)
+        self.linear = nn.Linear(hidden_dim, D_out)
+        self.hidden = self.init_hidden()
+
+    def init_hidden(self):
+        return (autograd.Variable(torch.zeros(self.num_layers, self.num_sequences, self.hidden_dim)),
+                autograd.Variable(torch.zeros(self.num_layers, self.num_sequences, self.hidden_dim)))
+
+    def forward(self,x):
+        out, self.hidden = self.lstm(x, self.hidden)
+        y_pred = self.linear(out)
+        return y_pred
+
 dtype = torch.FloatTensor
 
 Epoch_size = 50
 Sequence_size = 12
 Step_size = 6
 
-# Define Neural Network architecture
-D_in, L_1_out, L_2_in, L_2_out, D_out = 128, 512, 512, 512, 128
+# Call function to load dataset
+input_seq, target = load_data(Sequence_size, Step_size) # 32x128x12 : 32 Sequences, 128 Rows, 12 Sequence length
 
-model = nn.Sequential(
-    nn.LSTM(D_in, L_1_out)
-    # nn.Dropout(p=0.5),
-    # nn.LSTM(L_2_in, L_2_out),
-    # nn.Linear(L_2_out, D_out)
-)
+# Reshape the tensor since PyTorch LSTM expects 3D Tensors
+input_seq = autograd.Variable(dtype(input_seq)).permute(2,0,1) # 12x32x128 : 12 Sequence length (12 columns), 32 Mini batch size, 128 input elements (data points)
+target = autograd.Variable(dtype(target)).permute(2,0,1) # 1x32x128 : Output label vector has size 1 (1 column)
+
+# Define Neural Network architecture
+D_in, hidden_dim, D_out, num_layers = 128, 512, 128, 2
+
+model = LSTM_Net(D_in, hidden_dim, num_layers, D_out, 1)
 
 # Define loss function
-loss_fn = nn.CrossEntropyLoss()
+loss_fn = nn.modules.loss.MSELoss()
 
 # Define optimizer object
 optimizer = optim.Adam(params=model.parameters(), lr = 1e-4)
 
-# Call function to load dataset
-input_seq, target = load_data(Sequence_size, Step_size)
-
-input_seq = autograd.Variable(dtype(input_seq))
-target = autograd.Variable(dtype(target))
-
-# Reshape the tensor since PyTorch LSTM expects 3D Tensors
-input_seq = input_seq.permute(2,0,1)
-target = target.permute(2,0,1)
-
-# Train the model
+# Train the model - 50 iterations will be done over the same dataset
 for t in range(Epoch_size):
+    loss = None
+    # Input dimension contains number of sequences 
+    for i in range(input_seq.shape[1]):
+        optimizer.zero_grad()
 
-    optimizer.zero_grad()
+        # Reset memory of LSTM
+        model.hidden = model.init_hidden()
 
-    # Run 1 forward pass on data set
-    target_pred = model(input_seq)
+        # Run 1 forward pass on data set
+        target_pred = model(input_seq[:,i,:])
 
-    # Compute loss function
-    loss = loss_fn(target_pred, target)
-    
+        # Compute loss function
+        loss = loss_fn(target_pred[-1], target[:,i,:].squeeze(0))
+
+        # Compute gradients and adjust parameters accordingly
+        loss.backward()
+        optimizer.step()
     ####### For brevity - comment this out later ########
     print(t, loss.data[0])
-
-    # Compute gradients and adjust parameters accordingly
-    loss.backward()
-    optimizer.step()
