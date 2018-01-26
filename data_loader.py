c6bc060d71461e430ff999fd0e45e1d2df483457
omarnasir <momar.nasir@gmail.com>
2018-01-27 01:23:31 +0200

    Added comments to LSTM

--- data_loader.py
+++ data_loader.py
@@ -12,33 +12,31 @@ def load_data(sequence_size, step_size):
     input_seq = []
     target = []
     
-    # for midifile in midifiles:
-    #     df = pd.read_csv(midifile, compression='gzip')
-    #     print("reading: " + midifile)
-    #     # Compute input vector size
-    #     input_range = (df.shape[1] - sequence_size) // step_size
+    for midifile in midifiles:
+        df = pd.read_csv(midifile, compression='gzip')
+        print("reading: " + midifile)
+        # Compute input vector size
+        input_range = (df.shape[1] - sequence_size) // step_size
 
-    #     for i in range(0,input_range,step_size):
-    #         input_seq.append(df.values[:, i:i+sequence_size].ravel().reshape(128,sequence_size))
-    #         target.append(df.values[:,i+sequence_size:i+sequence_size+1].ravel().reshape(128,1))
-
-    # print("Completed")
-    # return input_seq, target
-
-    # To avoid memory leak
-    midifiles = midifiles[0]
-    
-    df = pd.read_csv(midifiles, compression='gzip')
-    print("reading: " + midifiles)
-    # Compute input vector size
-    input_range = (df.shape[1] - sequence_size) // step_size
-
-    for i in range(1,input_range,step_size):
-        input_seq.append(df.values[:, i:i+sequence_size].ravel().reshape(128,sequence_size))
-        target.append(df.values[:,i+sequence_size:i+sequence_size+1].ravel().reshape(128,1))
+        for i in range(0,input_range,step_size):
+            input_seq.append(df.values[:, i:i+sequence_size].ravel().reshape(128,sequence_size))
+            target.append(df.values[:,i+sequence_size:i+sequence_size+1].ravel().reshape(128,1))
 
     print("Completed")
-    return input_seq, np.array(target,dtype=int)
+    return input_seq, target
 
-if __name__ == "__main__":
-    load_data(12, 6)
+    # -------------To avoid memory leak
+    # -------------Dont run in final version
+    # midifiles = midifiles[0]
+    
+    # df = pd.read_csv(midifiles, compression='gzip')
+    # print("reading: " + midifiles)
+    # # Compute input vector size
+    # input_range = (df.shape[1] - sequence_size) // step_size
+
+    # for i in range(1,input_range,step_size):
+    #     input_seq.append(df.values[:, i:i+sequence_size].ravel().reshape(128,sequence_size))
+    #     target.append(df.values[:,i+sequence_size:i+sequence_size+1].ravel().reshape(128,1))
+
+    # print("Completed")
+    # return input_seq, np.array(target,dtype=int)
--- lstm.py
+++ lstm.py
@@ -7,68 +7,88 @@ import torch.optim as optim
 
 from data_loader import load_data
 
+# Define custom LSTM Class
+# Required because PyTorch method of chaining NN layers has issues with LSTMs
 class LSTM_Net(nn.Module):
     def __init__(self, D_in, hidden_dim, num_layers, D_out, num_sequences):
         super(LSTM_Net,self).__init__()
+        # Assign Hyperparameter values to class variables
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
         self.num_sequences = num_sequences
 
+        # Build LSTM NN with 2 Layers using dropout regularizer
         self.lstm = nn.LSTM(D_in, hidden_dim, num_layers, dropout=0.2)
+        # Add Linear Densely connected NN to transform 512 LSTM nodes to a vector of 128 outputs (pitches)  
         self.linear = nn.Linear(hidden_dim, D_out)
+        # Assign hidden layer shared variables, for gradient computations (backward pass)
         self.hidden = self.init_hidden()
 
     def init_hidden(self):
         return (autograd.Variable(torch.zeros(self.num_layers, self.num_sequences, self.hidden_dim)),
                 autograd.Variable(torch.zeros(self.num_layers, self.num_sequences, self.hidden_dim)))
 
+    # Define computation graph for Forward pass
     def forward(self,x):
+        # LSTM returns cell state and hidden state at time t
         out, self.hidden = self.lstm(x, self.hidden)
+        # We use total hidden state as input to Linear Layer
         y_pred = self.linear(out)
+        # Return predictions
         return y_pred
 
-dtype = torch.FloatTensor
-
+# Define hyperparameters
 Epoch_size = 50
-Sequence_size = 12
+Sequence_size = 9
 Step_size = 6
+D_in, hidden_dim, D_out, num_layers = 128, 512, 128, 2 # Neural Network architecture
 
 # Call function to load dataset
-input_seq, target = load_data(Sequence_size, Step_size) # 32x128x12 : 32 Sequences, 128 Rows, 12 Sequence length
+# num_sequences is also called batch size
+# num_pitches will always be 128
+# Dimensions of input_seq will be num_sequences x num_pitches x sequence_length
+# Dimensions of target will be num_sequences x num_pitches x 1
+input_seq, target = load_data(Sequence_size, Step_size) 
 
 # Reshape the tensor since PyTorch LSTM expects 3D Tensors
-input_seq = autograd.Variable(dtype(input_seq)).permute(2,0,1) # 12x32x128 : 12 Sequence length (12 columns), 32 Mini batch size, 128 input elements (data points)
-target = autograd.Variable(dtype(target)).permute(2,0,1) # 1x32x128 : Output label vector has size 1 (1 column)
+# LSTM expects input sequence containing 128 elements. 
+# Reshape input to sequence_length x num_sequences x num_pitches
+# Reshape output to 1 x num_sequences x num_pitches
+# dtype converts input python data to compatible PyTorch Tensors 
+dtype = torch.FloatTensor
+input_seq = autograd.Variable(dtype(input_seq)).permute(2,0,1) 
+target = autograd.Variable(dtype(target)).permute(2,0,1) 
 
-# Define Neural Network architecture
-D_in, hidden_dim, D_out, num_layers = 128, 512, 128, 2
-
-model = LSTM_Net(D_in, hidden_dim, num_layers, D_out, 1)
+# Build computation graph
+model = LSTM_Net(D_in, hidden_dim, num_layers, D_out, input_seq.size(1))
 
 # Define loss function
 loss_fn = nn.modules.loss.MSELoss()
 
 # Define optimizer object
-optimizer = optim.Adam(params=model.parameters(), lr = 1e-4)
+optimizer = optim.Adam(params=model.parameters(), lr = 1e-3)
 
 # Train the model - 50 iterations will be done over the same dataset
 for t in range(Epoch_size):
-    loss = None
-    # Input dimension contains number of sequences 
-    for i in range(input_seq.shape[1]):
-        optimizer.zero_grad()
+    
+    # From PyTorch docs - Gradient needs to be reset to 0 after every iteration 
+    # to avoid accumulated gradients
+    optimizer.zero_grad()
 
-        # Reset memory of LSTM
-        model.hidden = model.init_hidden()
+    # Reset memory of LSTM hidden layer
+    model.hidden = model.init_hidden()
+    
+    # Run 1 forward pass on data set
+    target_pred = model(input_seq)
 
-        # Run 1 forward pass on data set
-        target_pred = model(input_seq[:,i,:])
+    # Compute loss function
+    # Returns predictions for every time instance. We only need the last prediction
+    # that was computed when all sequences had been passed through LSTM
+    loss = loss_fn(target_pred[-1], target)
 
-        # Compute loss function
-        loss = loss_fn(target_pred[-1], target[:,i,:].squeeze(0))
+    # Compute gradients and adjust parameters accordingly
+    loss.backward(retain_graph=True)
+    optimizer.step()
 
-        # Compute gradients and adjust parameters accordingly
-        loss.backward()
-        optimizer.step()
     ####### For brevity - comment this out later ########
     print(t, loss.data[0])
